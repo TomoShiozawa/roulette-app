181f45810e4de1175e4e3ea190007edb01b51169
shiotomo <subtle.tomo@gmail.com>
2020-11-01 00:26:19 +0900

    chore: Add .eslint.js.

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    parser: 'babel-eslint',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {},
+}
