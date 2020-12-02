d6d697e937afdc32459873caf1640226bbbab2ec
shiotomo <subtle.tomo@gmail.com>
2020-12-02 23:53:59 +0900

    refactor: Change layout.

--- src/components/Raffle.vue
+++ src/components/Raffle.vue
@@ -1,21 +1,28 @@
 <template>
   <h1>Raffle</h1>
-  <div class="p-grid">
-    <div class="p-col-8 p-offset-2">
-      <TextArea v-model="itemText" rows="20" class="width-100" />
+  <div class="p-grid nested-grid">
+    <div class="p-col-6">
+      <div class="p-col-12">
+        <label for="itemtext">抽選するものを入力(改行区切り)</label>
+        <TextArea
+          id="itemtext"
+          v-model="itemText"
+          rows="20"
+          class="width-100"
+        />
+      </div>
+      <div class="p-col-12">
+        <Button label="抽選" class="width-100" @click="raffle" />
+      </div>
+    </div>
+    <div class="p-col-6">
+      <h2 class="p-text-center">
+        選ばれたのは<br />
+        {{ result }}<br />
+        でした
+      </h2>
     </div>
   </div>
-  <div class="p-grid">
-    <div class="p-col-6 p-offset-3">
-      <Button label="抽選" class="width-100" @click="raffle" />
-    </div>
-  </div>
-
-  <h2 class="p-text-center">
-    選ばれたのは<br />
-    {{ result }}<br />
-    でした
-  </h2>
 </template>
 
 <script>
@@ -30,6 +37,23 @@ export default {
   computed: {
     items() {
       return this.itemText.split('\n');
+    },
+    pieChartData() {
+      return {
+        labels: this.items,
+        datasets: [
+          {
+            data: this.items.map(() => 1),
+            backgroundColor: this.items.map(
+              () =>
+                '#' +
+                `000000${Math.floor(Math.random() * 16777215).toString(
+                  16
+                )}`.slice(-6)
+            ),
+          },
+        ],
+      };
     },
   },
   methods: {
