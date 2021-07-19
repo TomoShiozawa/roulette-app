ce98c73e951c2b27440a18501fed931e08fb96ec
shiotomo <subtle.tomo@gmail.com>
2021-07-20 00:01:58 +0900

    feat: Add timer component.

--- src/components/TabMenu.vue
+++ src/components/TabMenu.vue
@@ -6,18 +6,23 @@
     <TabPanel header="進数変換">
       <base-converter />
     </TabPanel>
+    <TabPanel header="タイマー">
+      <Timer />
+    </TabPanel>
   </TabView>
 </template>
 
 <script>
 import Raffle from './raffle/Raffle.vue';
 import BaseConverter from './baseconverter/BaseConverter.vue';
+import Timer from './timer/Timer.vue';
 
 export default {
   name: 'App',
   components: {
     Raffle,
     BaseConverter,
+    Timer,
   },
 };
 </script>
---
+++ src/components/timer/Timer.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2>タイマー</h2>
+  <div class="p-grid nested-grid">
+    <div class="p-col-12">
+      {{ formatTime }}
+    </div>
+    <div class="p-col-12 p-d-flex p-jc-center">
+      <div class="p-col-4">
+        <label for="hour">時</label>
+        <InputText id="hour" v-model="hour" />
+      </div>
+      <div class="p-col-4">
+        <label for="hour">分</label>
+        <InputText id="minute" v-model="minute" />
+      </div>
+      <div class="p-col-4">
+        <label for="hour">秒</label>
+        <InputText id="second" v-model="second" />
+      </div>
+    </div>
+    <div class="p-col-12 p-d-flex p-jc-center">
+      <div class="p-col-4">
+        <Button label="START" class="width-100" @click="raffle" />
+      </div>
+      <div class="p-col-4">
+        <Button label="START" class="width-100" @click="raffle" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from '@vue/runtime-core';
+export default {
+  name: 'Timer',
+  setup() {
+    const hour = ref('00');
+    const minute = ref('00');
+    const second = ref('00');
+
+    const formatTime = computed(() => {
+      return `${hour.value}:${minute.value}:${second.value}`;
+    });
+
+    return {
+      hour,
+      minute,
+      second,
+      formatTime,
+    };
+  },
+};
+</script>
+
+<style></style>
